a616201dc5f6ea889cfe0a467366c62690cd59b5
shumizu418128 <84061748+shumizu418128@users.noreply.github.com>
2022-06-29 21:37:18 +0900

    Update discordbot_image.py

--- discordbot_image.py
+++ discordbot_image.py
@@ -14,7 +14,8 @@ from oauth2client.service_account import ServiceAccountCredentials
 from PIL import Image
 from scipy.spatial import distance
 
-print(pyocr.__version__)
+from importlib_metadata import version
+print(version('pyocr'))
 def get_credits():
     return ServiceAccountCredentials.from_json_keyfile_name(
         "makesomenoise-4243a19364b1.json",
