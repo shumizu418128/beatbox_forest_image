aa4c67b3c6d76a98163a58e8d4c5680b91cca9b3
shumizu418128 <84061748+shumizu418128@users.noreply.github.com>
2023-06-06 21:21:05 +0900

    Update discordbot_image.py

--- discordbot_image.py
+++ discordbot_image.py
@@ -13,7 +13,7 @@ print(f'画像分析: {discord.__version__}')
 
 @client.event
 async def on_message(message):
-    submit = message.guild.get_channel(897784178958008322)  # bot用チャット
+    # submit = message.guild.get_channel(897784178958008322)  # bot用チャット
     if message.author.bot:
         return
 
@@ -21,7 +21,7 @@ async def on_message(message):
         await message.channel.send(f"{str(client.user)}\n{discord.__version__}")
         return
 
-    if len(message.attachments) != 2 and message.channel == submit:  # 画像提出
+    """if len(message.attachments) != 2 and message.channel.id == 897784178958008322:  # bot用チャット
         admin = message.author.get_role(904368977092964352)  # ビト森杯運営
         if bool(admin):
             return
@@ -29,9 +29,9 @@ async def on_message(message):
         await message.channel.send(f"{message.author.mention}\nError: 画像を2枚同時に投稿してください。", delete_after=5)
         if len(message.attachments) == 1:
             await message.channel.send("画像1枚では、すべての設定項目が画像内に収まりません。", delete_after=5)
-        return
+        return"""
 
-    if len(message.attachments) == 2 and message.channel == submit:  # 画像提出
+    if len(message.attachments) == 2 and message.channel.id == 897784178958008322:  # 画像提出
         await analyze(message)
 
 client.run(TOKEN)
