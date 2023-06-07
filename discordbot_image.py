e1ba71a0a722fb60b727d88a2513bee154306c39
shumizu418128 <84061748+shumizu418128@users.noreply.github.com>
2023-06-08 00:37:38 +0900

    Update discordbot_image.py

--- discordbot_image.py
+++ discordbot_image.py
@@ -36,7 +36,7 @@ async def on_message(message: Message):
             await message.channel.send("画像1枚では、すべての設定項目が画像内に収まりません。", delete_after=5)
         return
 
-    if len(message.attachments) == 2 and channel_id == [1115986804026392627, 897784178958008322]:  # マイクチェックチャンネル bot用チャット
+    if len(message.attachments) == 2 and channel_id in [1115986804026392627, 897784178958008322]:  # マイクチェックチャンネル bot用チャット
         await analyze(message)
 
 
