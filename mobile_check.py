56eac3aef6b44d48b189335defa37fcf16d12aaa
shumizu418128 <shumizu418128@gmail.com>
2023-06-07 14:45:40 +0900

    Update mobile_check.py

--- mobile_check.py
+++ mobile_check.py
@@ -88,6 +88,7 @@ async def text_check(file_names: list[str], log: str):  # 各種設定項目チ�
                 text_coordinate = [text_position[1][0], text_position[1][1]]
                 mobile_voice_overlay.append(text_coordinate)
                 log += "オーバーレイ座標: " + str(text_coordinate) + "\n"
+        mobile_voice_overlay.append("split")
     return [all_text, mobile_voice_overlay, log]
 
 
